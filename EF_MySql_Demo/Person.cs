d044a434ce04b0c43eb2b7c3fb7f727652baf750
Starinin Andrey <starininandrey@gmail.com>
2022-09-23 19:45:25 +0300

    Add Person.cs

---
+++ EF_MySql_Demo/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace EF_MySql_Demo;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+}

44e21e24ba4371fa5ba5a4e34d804a577d9d499d
Starinin Andrey <starininandrey@gmail.com>
2022-09-23 19:45:35 +0300

    Add DB.cs

---
+++ EF_MySql_Demo/DB.cs
@@ -0,0 +1,19 @@
+﻿using DbConfigLib;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_MySql_Demo;
+
+public class DB : DbContext
+{
+    public DbSet<Person> TablePersons => Set<Person>();
+    
+    public DB()
+    {
+        Database.EnsureCreated();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL(DbConfig.ImportFromJson("db.json").ToString());
+    }
+}

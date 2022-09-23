9831c48dace78302fe3babe693000ca083c0fb7f
Starinin Andrey <starininandrey@gmail.com>
2022-09-23 20:55:06 +0300

    proj

--- EF_MySql_Demo/Program.cs
+++ EF_MySql_Demo/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EF_MySql_Demo;
 
-Console.WriteLine("Hello, World!");
+var db = new DB();
+db.TablePersons.Add(new Person
+{
+    FirstName = "Andrey",
+    LastName = "Starinin",
+    Age = 36
+});
+db.SaveChanges();
+
+foreach (var p in db.TablePersons)
+{
+    Console.WriteLine($"{p.Id}: {p.LastName} {p.FirstName}, {p.Age}");
+}
+Console.WriteLine("--- --- ---");
+
+/*var person = db.TablePersons.Find(2);
+Console.WriteLine($"{person.Id}: {person.LastName} {person.FirstName}, {person.Age}");
+person.LastName = "Anonimus";
+person.FirstName = "Anonim";
+db.SaveChanges();
+Console.WriteLine($"{person.Id}: {person.LastName} {person.FirstName}, {person.Age}");*/
